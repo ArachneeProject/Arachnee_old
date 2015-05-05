4f7ccbf2e1be228147130da500901a98bc330e6d
SuperValou <valentin.esmieu@insa-rennes.fr>
2015-05-05 18:28:59 +0200

    runtime-generated list of toggle buttons

---
+++ ArachneeUnity/Assets/Scripts/databaseManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class databaseManager : MonoBehaviour 
+{
+    public InputField nameIF;
+    public InputField surnameIF;
+    public InputField yearIF;
+
+    public ToggleGroup toggleGrp;
+    public GameObject toggleBtnPrefab;
+    public Transform toogleSpawPoint;
+    public int spaceBetweenToggles = 1;
+
+    private List<string> jobsNames = new List<string>(); // just for tests
+    
+    void Start()
+    {
+        // just for tests
+        this.jobsNames.Add("Director");
+        this.jobsNames.Add("Actor");
+        this.jobsNames.Add("Toilet guy");
+        this.jobsNames.Add("Motherfucker");
+
+        for (int i = 0; i<jobsNames.Count ; i++)
+        {
+            GameObject toggleObject = (GameObject)Instantiate(toggleBtnPrefab, toogleSpawPoint.position + -1 * spaceBetweenToggles * i * Vector3.up, Quaternion.identity);
+            toggleObject.transform.SetParent(toggleGrp.transform);
+            Toggle toggleScript = toggleObject.GetComponent<Toggle>();
+            toggleScript.group = toggleGrp;
+            toggleScript.isOn = false;
+            Text textInToggle = toggleScript.GetComponentInChildren<Text>();
+            Debug.Log(textInToggle.text);
+            textInToggle.text = jobsNames[i];
+        }
+        
+        
+    }
+
+
+    public void SubmitName()
+    {
+        Debug.Log(nameIF.text + " " + surnameIF.text + " (" + yearIF.text + ")");
+    }
+}
