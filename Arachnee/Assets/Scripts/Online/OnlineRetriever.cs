976d29f619cf3c6f6e84de1bebd74dbd1a5bf85b
Aya <valentin.esmieu@insa-rennes.fr>
2015-10-31 23:07:28 +0100

    separation between menu and online retriver

--- Arachnee/Assets/Scripts/Online/OnlineRetriever.cs
+++ Arachnee/Assets/Scripts/Online/OnlineRetriever.cs
@@ -1,44 +1,26 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
 using System;
 using SimpleJSON;
-using UnityEngine.UI;
+using UnityEngine;
 
-public class OnlineRetriever : MonoBehaviour 
+public class OnlineRetriever 
 {
-    //private string testurl = "https://api.themoviedb.org/3/movie/550?api_key=The Skeleton Key";
-    //private string testurlSerach = "https://api.themoviedb.org/3/search/movie?query=cube&api_key=The Skeleton Key";
+    //private string testurl = "https://api.themoviedb.org/3/movie/550?api_key= x x x";
+    //private string testurlSerach = "https://api.themoviedb.org/3/search/movie?query=cube&api_key= x x x";
     //private string testposterUrl = "http://image.tmdb.org/t/p/w500/u50r6chJGO2iqxVAvtQ07obiCkB.jpg";
 
-    public InputField inputField;
-    public MovieSearchResult[] movieResults;
-    public Text loading;
-    public Text overviewBox;
+    
 
     private string urlSearch = "https://api.themoviedb.org/3/search/movie?query=";
     private string posterUrl = "http://image.tmdb.org/t/p/w500";
+    private string apiKey = " x x x ";
 
-    private string apiKey = " x x x x ";
-
-
-    /// <summary>
-    /// go get the movies online
-    /// </summary>
-    public void Retrieve()
+    public JSONNode LastJSONNodeRetrieved
     {
-        this.resetFields();
-        string processedInput = this.processInput(this.inputField.text);
-        StartCoroutine(this.retrieveResultOfSerach(processedInput));
+        get;
+        private set;
     }
 
-    private void resetFields()
-    {
-        for (int i = 0; i<this.movieResults.Length ; i++)
-        {
-            this.movieResults[i].Image.sprite = null;
-            this.movieResults[i].Title.text = "-";
-        }
-    }
 
     /// <summary>
     /// modify the input field to be sent as a query
@@ -50,40 +32,26 @@ public class OnlineRetriever : MonoBehaviour
         return text.Replace(" ","%20");
     }
 
-
-    private IEnumerator retrieveResultOfSerach(string input)
+    /// <summary>
+    /// Retrieve JSON info
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="node"></param>
+    /// <returns></returns>
+    public IEnumerator RetrieveResultOfSerach(string input)
     {
-        this.loading.gameObject.SetActive(true);
+        input = this.processInput(input);
 
         WWW www = new WWW(this.urlSearch + input + this.apiKey);
+        Debug.Log(this.urlSearch + input + this.apiKey);
         yield return www;
 
         JSONNode node = JSON.Parse(www.text);
-        if (node["results"] == null)
+        if (this.LastJSONNodeRetrieved == null)
         {
-            Logger.Trace("No result", LogLevel.Error);
-            this.loading.gameObject.SetActive(false);
-            yield break;
+            this.LastJSONNodeRetrieved = new JSONNode();
         }
-
-        JSONNode resultsNode = node["results"];
-        for (int i = 0; i < resultsNode.Count && i < this.movieResults.Length; i++)
-        {
-            this.movieResults[i].Title.text = resultsNode[i]["original_title"].Value;
-            this.movieResults[i].Overview = resultsNode[i]["overview"].Value;
-
-            string posterUrl = resultsNode[i]["poster_path"].Value;
-            if (posterUrl != "null")
-            {
-                StartCoroutine(this.retrievePoster(posterUrl, this.movieResults[i].Image));
-            }
-            else
-            {
-                Logger.Trace("No poster found", LogLevel.Warning);
-                this.loading.gameObject.SetActive(false);
-            }
-            
-        }
+        this.LastJSONNodeRetrieved = node["results"];
     }
 
 
@@ -93,23 +61,17 @@ public class OnlineRetriever : MonoBehaviour
     /// <param name="posterPath"></param>
     /// <param name="img"></param>
     /// <returns></returns>
-    private IEnumerator retrievePoster(string posterPath, Image img)
+    public IEnumerator RetrievePoster(string posterPath, UnityEngine.UI.Image img)
     {
         WWW www = new WWW(this.posterUrl + posterPath);
-
         yield return www;
 
         if (www.texture != null && img != null)
         {
             img.sprite = Sprite.Create(www.texture, new Rect(0, 0, www.texture.width, www.texture.height), Vector2.zero);
         }
-
-        this.loading.gameObject.SetActive(false);
     }
 
 
-    public void SelectResult(string overview)
-    {
-        this.overviewBox.text = overview;
-    }
+    
 }
---
+++ Arachnee/Assets/Scripts/Online/StartMenu.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using SimpleJSON;
+
+public class StartMenu : MonoBehaviour 
+{
+    public InputField inputField;
+    public MovieSearchResult[] movieResults;
+    public Text loading;
+
+    private OnlineRetriever onlineRetriever = new OnlineRetriever();
+
+    
+    /// <summary>
+    /// public function for the button in menu
+    /// </summary>
+    public void GoGetResults()
+    {
+        StartCoroutine(this.goGetResultsOnline());
+    }
+
+    /// <summary>
+    /// go get the movies online
+    /// </summary>
+    private IEnumerator goGetResultsOnline()
+    {
+        this.resetFields();
+
+        yield return StartCoroutine(this.onlineRetriever.RetrieveResultOfSerach(this.inputField.text));
+
+        JSONNode resultsNode = this.onlineRetriever.LastJSONNodeRetrieved;
+
+        Debug.Log("Checking result...");
+        if (resultsNode == null)
+        {
+            Logger.Trace("No result", LogLevel.Info);
+            yield break;
+        }
+
+        for (int i = 0; i < resultsNode.Count && i < this.movieResults.Length; i++)
+        {
+            this.movieResults[i].Title.text = resultsNode[i]["original_title"].Value;
+            this.movieResults[i].Overview = resultsNode[i]["overview"].Value;
+
+            string posterUrl = resultsNode[i]["poster_path"].Value;
+            if (posterUrl != "null")
+            {
+                yield return StartCoroutine(this.onlineRetriever.RetrievePoster(posterUrl, this.movieResults[i].Image));
+            }
+            else
+            {
+                Logger.Trace("No poster found", LogLevel.Warning);
+                this.loading.gameObject.SetActive(false);
+            }
+        }
+    }
+
+
+    private void resetFields()
+    {
+        for (int i = 0; i < this.movieResults.Length; i++)
+        {
+            this.movieResults[i].Image.sprite = null;
+            this.movieResults[i].Title.text = "-";
+        }
+    }
+
+}
