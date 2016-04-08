6f415807656610fd25f161101ecd228634b33d59
SuperValou <valentin.esmieu@insa-rennes.fr>
2016-04-08 21:26:56 +0200

    few details

--- Arachnee/Assets/Scripts/Constants.cs
+++ Arachnee/Assets/Scripts/Constants.cs
@@ -4,12 +4,13 @@ using System.Collections;
 public class Constants 
 {
     // scene numbers
-    public const int SC_FOLDER_SETTING = 0;
-    public const int SC_FOLDER_CHECK = 1;
-    public const int SC_BROWSER = 2;
-    public const int SC_INTERNET_MOVIE_SEARCH = 3;
-    public const int SC_DATABASE_UPDATE = 4;
-    public const int SC_GRAPH = 5;
+	public const int SC_MAIN_MENU = 0;
+    public const int SC_FOLDER_SETTING = 1;
+    public const int SC_FOLDER_CHECK = 2;
+    public const int SC_BROWSER = 3;
+    public const int SC_INTERNET_MOVIE_SEARCH = 4;
+    public const int SC_DATABASE_UPDATE = 5;
+    public const int SC_GRAPH = 6;
 
     // PlayerPrefs names
     public const string PP_MOVIE_ID = "MovieID";
---
+++ Arachnee/Assets/Scripts/Online/DatabaseUpdate.cs
@@ -0,0 +1,284 @@
+﻿using UnityEngine;
+using System.Collections;
+using SimpleJSON;
+using System.Collections.Generic;
+using System;
+using UnityEngine.UI;
+
+public class DatabaseUpdate : MonoBehaviour
+{
+    public Text message;
+    private DatabaseDialoger dlg = new DatabaseDialoger();
+    private OnlineRetriever onlRet = new OnlineRetriever();
+
+    private HashSet<long> moviesToDo = new HashSet<long>();
+    private HashSet<long> artistsToDo = new HashSet<long>();
+
+
+    void Start()
+    {
+        this.message.text = "Initialization...";
+        this.dlg.Initialize();
+
+        StartCoroutine(this.mainCoroutine());
+    }
+
+
+
+    // main routine
+    private IEnumerator mainCoroutine()
+    {
+        // load what to do
+        loadToDo();
+        if (this.moviesToDo.Count == 0)
+        {
+            yield break;
+        }
+
+        // foreach movie to do
+        foreach (long idMovie in this.moviesToDo)
+        {
+            // add the movie to the database
+            yield return StartCoroutine(addMovieToDatabase(idMovie));
+
+            // if movie successfully added, add casting
+            if (this.dlg.CheckIfEntryExistsInTable(idMovie, "movies"))
+            {
+                yield return StartCoroutine(this.addCasting(idMovie));
+            }
+        }
+        this.moviesToDo.Clear();
+
+        // foreach artist to do
+        foreach (long idArt in this.artistsToDo)
+        {
+            // add the artist to the database
+            yield return StartCoroutine(addArtistToDatabase(idArt));
+
+            // if artist successfully added, add credits
+            if (this.dlg.CheckIfEntryExistsInTable(idArt, "artists"))
+            {
+                yield return StartCoroutine(this.addCredits(idArt));
+            }
+        }
+        this.artistsToDo.Clear();
+
+        // save what to do next time
+        saveToDo();
+    }
+
+    // load what needs to be done
+    void loadToDo()
+    {
+        long seed = PlayerPrefs.GetInt(Constants.PP_MOVIE_ID);
+
+        // if the seed is present in database, load the scene
+        if (this.dlg.CheckIfEntryExistsInTable(seed, "movies"))
+        {
+            this.message.text = "Launching scene...";
+            Application.LoadLevel(Constants.SC_GRAPH);
+        }
+        else
+        {
+            // else let the routine run
+            this.moviesToDo.Add(PlayerPrefs.GetInt(Constants.PP_MOVIE_ID));
+            Logger.Trace("ID of movie: " + PlayerPrefs.GetInt(Constants.PP_MOVIE_ID), LogLevel.Debug);
+        }
+    }
+
+
+
+    // Insert the movie and all infos about it in database (only if it has a poster)
+    private IEnumerator addMovieToDatabase(long movieId)
+    {
+        this.message.text = "Loading...";
+
+        yield return StartCoroutine(onlRet.RetrieveMovie(movieId));
+        JSONNode node = onlRet.NodeRetrieved;
+        if (node == null)
+        {
+            Debug.LogError("No movie was found with the id " + movieId);
+            yield break;
+        }
+
+        string posterPath = node["poster_path"].Value;
+        if (posterPath == "null")
+        {
+            yield break;
+        }
+        string title = node["original_title"].Value;
+        this.message.text = title;
+
+        Int64 date = 1;
+        string d = node["release_date"].Value;
+        if (d.Length > 4)
+        {
+            date = Convert.ToInt64(d.Substring(0, 4));
+        }
+        else
+        {
+            Debug.LogError("date is wrong: " + node["release_date"].Value + " (on id " + movieId + ")");
+            yield break;
+        }
+
+        this.dlg.InsertMovie(movieId, title, date, posterPath);
+    }
+
+
+    // insert the connections between the given movie and the artists already existing in the database
+    private IEnumerator addCasting(long movieId)
+    {
+        yield return StartCoroutine(this.onlRet.RetrieveCast(movieId));
+        JSONNode node = this.onlRet.NodeRetrieved;
+        if (node == null)
+        {
+            Debug.LogError("No casting was found about the movie with the id " + movieId);
+            yield break;
+        }
+
+        // for each actor in the movie
+        for (int i = 0; i < node["cast"].Count; i++)
+        {
+            long artistId = Convert.ToInt64(node["cast"][i]["id"].Value);
+
+            // if artist exists in db
+            if (this.dlg.CheckIfEntryExistsInTable(artistId, "artists"))
+            {
+                // add the connection
+                this.dlg.InsertConnection(artistId, movieId, "actors");
+            }
+            else
+            {
+                // else add to ToDo list
+                this.artistsToDo.Add(artistId);
+            }
+        }
+
+        // for each director in the movie
+        for (int i = 0; i < node["crew"].Count; i++)
+        {
+            if (node["crew"][i]["job"].Value == "Director")
+            {
+                long artistId = Convert.ToInt64(node["crew"][i]["id"].Value);
+
+                // if artist exists in db
+                if (this.dlg.CheckIfEntryExistsInTable(artistId, "artists"))
+                {
+                    // add the connection
+                    this.dlg.InsertConnection(artistId, movieId, "directors");
+                }
+                // else add to ToDo list
+                else
+                {
+                    this.artistsToDo.Add(artistId);
+                }
+            }
+        }
+    }
+
+
+    /// Insert the artist's infos in the database (only if he/she has a poster)
+    private IEnumerator addArtistToDatabase(long artistId)
+    {
+        this.message.text = "Loading...";
+
+        yield return StartCoroutine(this.onlRet.RetrieveArtist(artistId));
+        JSONNode node = this.onlRet.NodeRetrieved;
+        if (node == null)
+        {
+            Debug.LogError("No artist was found with the id " + artistId);
+            yield break;
+        }
+
+        string posterPath = node["profile_path"].Value;
+        if (posterPath == "null")
+        {
+            yield break;
+        }
+
+        string name = node["name"].Value;
+        this.message.text = name;
+        string firstName = "";
+        string lastName = "";
+
+        int idx = name.LastIndexOf(' ');
+        if (idx < 0)
+        {
+            lastName = name;
+            // also known as...
+        }
+        else
+        {
+            firstName = name.Substring(0, idx);
+            lastName = name.Substring(idx + 1);
+        }
+
+        this.dlg.InsertArtist(artistId, firstName, lastName, posterPath);
+    }
+
+
+    /// Insert the connections between the given artist and the movies already existing in the database
+    private IEnumerator addCredits(long artistI)
+    {
+        yield return StartCoroutine(this.onlRet.RetrieveCredits(artistI));
+        JSONNode node = this.onlRet.NodeRetrieved;
+        if (node == null)
+        {
+            Debug.LogError("No credits were found about the artist with the id " + artistI);
+            yield break;
+        }
+
+
+        // for each movie the artist played in
+        for (int i = 0; i < node["cast"].Count; i++)
+        {
+            var mvId = Convert.ToInt64(node["cast"][i]["id"].Value);
+
+            // if the movie exists
+            if (this.dlg.CheckIfEntryExistsInTable(mvId, "movies"))
+            {
+                // insert the connection
+                this.dlg.InsertConnection(artistI, mvId, "actors");
+            }
+            else
+            {
+                // else add to ToDo list
+                this.moviesToDo.Add((int)mvId);
+            }
+        }
+
+        // for each movie the artist directed
+        for (int i = 0; i < node["crew"].Count; i++)
+        {
+            if (node["crew"][i]["job"].Value == "Director")
+            {
+                Int64 mvId = Convert.ToInt64(node["crew"][i]["id"].Value);
+
+                // if the movie exists
+                if (this.dlg.CheckIfEntryExistsInTable(mvId, "movies"))
+                {
+                    // add the connection
+                    this.dlg.InsertConnection(artistI, mvId, "directors");
+                }
+                else
+                {
+                    // add to ToDoList
+                    this.moviesToDo.Add((int)mvId);
+                }
+            }
+        }
+
+
+    }
+
+
+    // save what needs to be done next time
+    void saveToDo()
+    {
+        this.message.text = "Launching scene...";
+        Application.LoadLevel(Constants.SC_GRAPH);
+    }
+
+
+    //  https://api.themoviedb.org/3/person/76489/movie_credits?api_key=The Skeleton Key
+}
