e558efaeeaaf5f87cec448cf21aacd1b73620281
SuperValou <valentin.esmieu@insa-rennes.fr>
2016-05-14 21:09:17 +0200

    freeSearch upgrade

--- Assets/Scripts/Menus/FreeSearch.cs
+++ Assets/Scripts/Menus/FreeSearch.cs
@@ -4,25 +4,32 @@ using System.Collections;
 public class FreeSearch : MonoBehaviour 
 {
     public InternetSearch internetSearch;
+    public Navigation nav;
 
     public void GoButton()
     {
         DatabaseDialoger dlg = new DatabaseDialoger();
-        
+
         var selected = this.internetSearch.movieSearch.results[internetSearch.SelectedResult];
 
-        dlg.InsertMovie(selected.Id, selected.Title, selected.Year, selected.PosterPath, false);
-        string toUpdate = PlayerPrefs.GetString(Constants.PP_MoviesToUpdate);
-        if (toUpdate == string.Empty)
-        {
-            toUpdate += selected.Id;
-        }
-        else
-        {
-            toUpdate += "," + selected.Id;
-        }
-        PlayerPrefs.SetString(Constants.PP_MoviesToUpdate, toUpdate);
-
         PlayerPrefs.SetInt(Constants.PP_MovieID, selected.Id);
+
+        if (!dlg.MovieIsRegistered(selected.Id))
+        {
+            dlg.InsertMovie(selected.Id, selected.Title, selected.Year, selected.PosterPath, false);
+            string toUpdate = PlayerPrefs.GetString(Constants.PP_MoviesToUpdate);
+            if (toUpdate == string.Empty)
+            {
+                toUpdate += selected.Id;
+            }
+            else
+            {
+                toUpdate += "," + selected.Id;
+            }
+            PlayerPrefs.SetString(Constants.PP_MoviesToUpdate, toUpdate);
+        }
+
+        nav.GoToDatabaseUpdate();        
     }
+
 }
