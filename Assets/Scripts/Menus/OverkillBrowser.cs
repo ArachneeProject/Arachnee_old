4c033f8cc1b4d089b5346456c919ad0021d9f1d0
SuperValou <valentin.esmieu@insa-rennes.fr>
2016-05-15 00:54:21 +0200

    more pleasant loading of folders

--- Assets/Scripts/Menus/OverkillBrowser.cs
+++ Assets/Scripts/Menus/OverkillBrowser.cs
@@ -62,6 +62,8 @@ public class OverkillBrowser : MonoBehaviour
             }
             
             pf.image.sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), Vector2.zero);
+
+            yield return new WaitForEndOfFrame();
         }
     }
 
